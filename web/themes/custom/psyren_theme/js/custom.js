cb356be37ca169fc9338de3a38988a329fc1fd78
Patrick Mandigo <patrick.mandigo@xterraplanet.com>
2019-06-30 21:13:40 -0700

    Theme improvments including a)add and configure share_everywhere module b) change contact to 'contact and book' c) zoom out moon graphic at top of page per PSYREN request

--- web/themes/custom/psyren_theme/js/custom.js
+++ web/themes/custom/psyren_theme/js/custom.js
@@ -1,44 +1,39 @@
 /* --------------------------------------------- 
-* Filename:     custom.js
-* Version:      1.0.0 (Tue 26 Mar 2019)
-* Website:      https://www.psyrenultimate.com
-* Description:  Global Script
-* Author:       psyrenultimate Team
-                info@psyrenultimate.com
+Custom JS
 -----------------------------------------------*/
 
-function theme_menu(){
+function theme_menu() {
 
 	//Main menu
 	jQuery('#main-menu').smartmenus();
-	
+
 	//Mobile menu toggle
-	jQuery('.navbar-toggle').click(function(){
+	jQuery('.navbar-toggle').click(function () {
 		jQuery('.region-primary-menu').addClass('expand');
 	});
-	jQuery('.navbar-toggle-close').click(function(){
+	jQuery('.navbar-toggle-close').click(function () {
 		jQuery('.region-primary-menu').removeClass('expand');
 	});
 
 	//Mobile drop-down menu
-	if ( jQuery(window).width() < 767) {
+	if (jQuery(window).width() < 767) {
 		jQuery(".region-primary-menu li a:not(.has-submenu)").click(function () {
 			jQuery('.region-primary-menu').hide();
-	    });
+		});
 	}
 
 }
 
-function theme_home(){
-	
+function theme_home() {
+
 	//flexslider
 	jQuery('.flexslider').flexslider({
-    	animation: "slide"	
-    });
+		animation: "slide"
+	});
 
 }
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	theme_menu();
 	theme_home();
 });
